b27900a22aa29998b669179aed1614741faf5d42
Matthew Peveler <matt.peveler@gmail.com>
2020-04-19 14:16:50 -0400

    finish up initial version of this

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# slack_travis_cleaner
+# slack_bot_cleaner
 
-This repo holds some utility code to delete old Travis-CI messages from the #travis-ci channel.
+This repo holds some utility code to delete messages from bots from from a channel.
+
+## Requirements
+
+* Python 3.6
+* [slack_cleaner2](https://pypi.org/project/slack-cleaner2/)
+
+## Usage
+
+```bash
+python3 clean_slack.py <slack_token> <channel_name>
+```
--- clean_slack.py
+++ clean_slack.py
@@ -1,34 +1,66 @@
 import argparse
 from datetime import datetime, timedelta
+import time
+from typing import Tuple
 
 from slack_cleaner2 import SlackCleaner, match
+import slacker
+
+
+API_THROTTLE = 30
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser("Clean Travis-CI slack messages")
     parser.add_argument('slack_token', type=str)
+    parser.add_argument('channel', type=str, default='travis-ci')
     return parser.parse_args()
 
 
-def clean_slack(token: str, max_count: int = 30) -> None:
-    slack = SlackCleaner(token)
-    count = 0
+def delete_messages(
+    slack: SlackCleaner,
+    channel: str,
+    count: int
+) -> Tuple[bool, int]:
     before = (datetime.now() - timedelta(days=7)).strftime('%Y%m%d')
     msgs = slack.msgs(
         filter(match('travis-ci'), slack.conversations),
         before=before
     )
-    for msg in msgs:
-        if msg.bot is True:
-            msg.delete()
-            count += 1
-            if count == max_count:
-                return
+    completed = True
+    try:
+        for msg in msgs:
+            if msg.bot is True:
+                msg.delete()
+                count += 1
+                if count % API_THROTTLE == 0:
+                    print(f"  {str(count).rjust(5, ' ')}")
+                    time.sleep(60)
+    except slacker.Error:
+        completed = False
+
+    return (completed, count)
+
+
+def clean_slack(token: str, channel: str) -> None:
+    slack = SlackCleaner(token)
+    count = 0
+
+    while True:
+        (completed, count) = delete_messages(slack, channel, count)
+        if completed:
+            break
+
+    print(str(count).rjust(2 + 5 + (API_THROTTLE - count), ' '))
+    print('')
+    print(f'Messages Deleted: {count}')
 
 
 def main() -> None:
     args = parse_args()
-    clean_slack(args.slack_token)
+    channel = args.channel.lstrip('#')
+    print(f"Deleting Travis-CI messages from #{channel}")
+    clean_slack(args.slack_token, channel)
 
 
 if __name__ == "__main__":
